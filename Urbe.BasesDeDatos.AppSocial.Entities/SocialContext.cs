cdec8ad3d186bbaba4cb49a74ff9d4c98d0cd8f6
Diego Garcia <dagarciam1014@gmail.com>
2023-10-12 20:34:08 -0400

    Add project files.

---
+++ Urbe.BasesDeDatos.AppSocial.Entities/SocialContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Urbe.BasesDeDatos.AppSocial.Entities;
+
+public class SocialContext : DbContext
+{
+    public SocialContext(DbContextOptions<SocialContext> options) : base(options)
+    {
+
+    }
+}
