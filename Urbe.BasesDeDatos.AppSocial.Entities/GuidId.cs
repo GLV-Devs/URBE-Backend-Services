278c5d117a3ec1a0ccc178fc32a40e4e3c3e8e97
Diego Garcia <dagarciam1014@gmail.com>
2023-10-13 00:54:40 -0400

    Introduced GuidId and SnowflakeId
    Renamed old SnowflakeId to Snowflake

---
+++ Urbe.BasesDeDatos.AppSocial.Entities/GuidId.cs
@@ -0,0 +1,19 @@
+﻿using Urbe.BasesDeDatos.AppSocial.Entities.Interfaces;
+
+namespace Urbe.BasesDeDatos.AppSocial.Entities;
+
+public readonly record struct GuidId<TEntity> where TEntity : class, IEntity
+{
+    public Guid Id { get; }
+
+    public GuidId(Guid id)
+    {
+        Id = id;
+    }
+
+    public static implicit operator GuidId<TEntity>(Guid id)
+        => new(id);
+
+    public static explicit operator Guid(GuidId<TEntity> id)
+        => id.Id;
+}
--- Urbe.BasesDeDatos.AppSocial.Entities/SnowflakeId.cs
+++ Urbe.BasesDeDatos.AppSocial.Entities/SnowflakeId.cs
@@ -1,120 +1,19 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices;
+﻿using Urbe.BasesDeDatos.AppSocial.Entities.Interfaces;
 
 namespace Urbe.BasesDeDatos.AppSocial.Entities;
 
-[StructLayout(LayoutKind.Explicit)]
-public readonly struct SnowflakeId : IEquatable<SnowflakeId>, IComparable<SnowflakeId>, IParsable<SnowflakeId>, IFormattable
+public readonly record struct SnowflakeId<TEntity> where TEntity : class, IEntity
 {
-    private readonly static long ReferenceStampUtc;
+    public Snowflake Id { get; }
 
-    static SnowflakeId()
+    public SnowflakeId(Snowflake id)
     {
-        ReferenceStampUtc = 638327520000000000; // October 13, 2023
+        Id = id;
     }
 
-    private static int LastStamp;
-    private static uint LastIndex;
+    public static implicit operator SnowflakeId<TEntity>(Snowflake id)
+        => new(id);
 
-    [FieldOffset(0)]
-    private readonly long aslong;
-
-    [FieldOffset(0)]
-    private readonly int timeStamp;
-
-    [FieldOffset(sizeof(int))]
-    private readonly uint index;
-
-    public SnowflakeId(int timeStamp, uint index) 
-    {
-        this.timeStamp = timeStamp;
-        this.index = index;
-    }
-
-    public SnowflakeId(long value)
-    {
-        aslong = value;
-    }
-
-    public static int GetSnowflakeTimeStamp()
-        => (int)(DateTime.UtcNow.Ticks - ReferenceStampUtc);
-
-    public static SnowflakeId New()
-    {
-        var stamp = GetSnowflakeTimeStamp();
-        if (stamp != LastStamp)
-        {
-            LastIndex = 0;
-            LastStamp = stamp;
-        }
-
-        return new SnowflakeId(stamp, LastIndex++);
-    }
-
-    public long AsLong() => aslong;
-    public DateTime TimeStamp => new(timeStamp + ReferenceStampUtc, DateTimeKind.Utc);
-    public uint Index => index;
-
-    public bool Equals(SnowflakeId other)
-        => aslong == other.aslong;
-
-    public int CompareTo(SnowflakeId other)
-        => aslong.CompareTo(other.aslong);
-
-    public static SnowflakeId Parse(string s, IFormatProvider? provider)
-        => new(long.Parse(s, provider));
-
-    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out SnowflakeId result)
-    {
-        if (long.TryParse(s, provider, out var value))
-        {
-            result = new(value);
-            return true;
-        }
-
-        result = default;
-        return false;
-    }
-
-    public string ToString(string? format, IFormatProvider? formatProvider)
-        => aslong.ToString(format, formatProvider);
-
-    public override string ToString()
-        => ToString(null, null);
-
-    public override bool Equals(object? obj)
-        => aslong.Equals(obj);
-
-    public override int GetHashCode()
-        => aslong.GetHashCode();
-
-    public static bool operator ==(SnowflakeId left, SnowflakeId right)
-    {
-        return left.Equals(right);
-    }
-
-    public static bool operator !=(SnowflakeId left, SnowflakeId right)
-    {
-        return !(left == right);
-    }
-
-    public static bool operator <(SnowflakeId left, SnowflakeId right)
-    {
-        return left.CompareTo(right) < 0;
-    }
-
-    public static bool operator <=(SnowflakeId left, SnowflakeId right)
-    {
-        return left.CompareTo(right) <= 0;
-    }
-
-    public static bool operator >(SnowflakeId left, SnowflakeId right)
-    {
-        return left.CompareTo(right) > 0;
-    }
-
-    public static bool operator >=(SnowflakeId left, SnowflakeId right)
-    {
-        return left.CompareTo(right) >= 0;
-    }
+    public static explicit operator Snowflake(SnowflakeId<TEntity> id)
+        => id.Id;
 }
