2b3ad220cdb9e34f5d4e482c693f43b02b63b1dc
Diego Garcia <dagarciam1014@gmail.com>
2023-10-13 22:58:00 -0400

    Update RandomKeyId.cs

--- Urbe.BasesDeDatos.AppSocial.Entities/RandomKeyId.cs
+++ Urbe.BasesDeDatos.AppSocial.Entities/RandomKeyId.cs
@@ -1,9 +1,14 @@
-﻿using Urbe.BasesDeDatos.AppSocial.Entities.Interfaces;
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Urbe.BasesDeDatos.AppSocial.Entities.Interfaces;
 
 namespace Urbe.BasesDeDatos.AppSocial.Entities;
 
-public readonly struct RandomKeyId<TEntity> : IConvertibleProperty
+public readonly record struct RandomKeyId<TEntity>(RandomKey Value)
+    : IConvertibleProperty
     where TEntity : class, IEntity
 {
-
+    public static ValueConverter ValueConverter { get; } = new ValueConverter<RandomKeyId<TEntity>, byte[]>(
+        x => x.Value.ToByteArray(),
+        x => new RandomKeyId<TEntity>(new RandomKey(x))
+    );
 }
