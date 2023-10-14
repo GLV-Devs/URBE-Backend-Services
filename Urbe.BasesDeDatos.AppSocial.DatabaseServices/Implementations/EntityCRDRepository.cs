e3e577773303f87b1db2ee211493ba0acdce8d70
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 15:12:54 -0400

    Created abstract Entity repositories

---
+++ Urbe.BasesDeDatos.AppSocial.DatabaseServices/Implementations/EntityCRDRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Common;
+using Urbe.BasesDeDatos.AppSocial.Common;
+using Urbe.BasesDeDatos.AppSocial.Entities;
+using Urbe.BasesDeDatos.AppSocial.Entities.Interfaces;
+using Urbe.BasesDeDatos.AppSocial.Entities.Models;
+
+namespace Urbe.BasesDeDatos.AppSocial.DatabaseServices.Implementations;
+
+public abstract class EntityCRDRepository<TEntity, TKey, TCreationModel> : EntityRepository<TEntity, TKey>, IEntityCRDRepository<TEntity, TKey, TCreationModel>
+    where TEntity : class, IEntity, IKeyed<TKey>
+    where TKey : struct, IEquatable<TKey>
+{
+    public EntityCRDRepository(SocialContext context) : base(context) { }
+
+    public abstract ValueTask<SuccessResult<TEntity>> Create(SocialAppUser requester, TCreationModel model);
+
+    public abstract ValueTask<object> GetView(SocialAppUser requester, TEntity entity);
+
+    public virtual ValueTask<bool> Delete(SocialAppUser requester, TEntity entity)
+    {
+        context.Set<TEntity>().Remove(entity);
+        return ValueTask.FromResult(true);
+    }
+}
---
+++ Urbe.BasesDeDatos.AppSocial.DatabaseServices/Implementations/EntityCRUDRepository.cs
@@ -0,0 +1,16 @@
+﻿using Urbe.BasesDeDatos.AppSocial.Common;
+using Urbe.BasesDeDatos.AppSocial.Entities;
+using Urbe.BasesDeDatos.AppSocial.Entities.Interfaces;
+using Urbe.BasesDeDatos.AppSocial.Entities.Models;
+
+namespace Urbe.BasesDeDatos.AppSocial.DatabaseServices.Implementations;
+
+public abstract class EntityCRUDRepository<TEntity, TKey, TCreationModel, TUpdateModel>
+    : EntityCRDRepository<TEntity, TKey, TCreationModel>, IEntityCRUDRepository<TEntity, TKey, TCreationModel, TUpdateModel>
+    where TEntity : class, IEntity, IKeyed<TKey>
+    where TKey : struct, IEquatable<TKey>
+{
+    protected EntityCRUDRepository(SocialContext context) : base(context) { }
+
+    public abstract ValueTask<ErrorList> Update(SocialAppUser requester, TEntity entity, TUpdateModel update);
+}
