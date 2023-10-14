a6bd81d61bdfc548be11419526891ac679128d0c
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 02:33:27 -0400

    Update Helper.cs

--- Urbe.BasesDeDatos.AppSocial.Common/Helper.cs
+++ Urbe.BasesDeDatos.AppSocial.Common/Helper.cs
@@ -8,6 +8,6 @@ using System.Threading.Tasks;
 namespace Urbe.BasesDeDatos.AppSocial.Common;
 public static class Helper
 {
-    public static bool IsUpdating(string original, [NotNullWhen(true)] string? update)
+    public static bool IsUpdating(string? original, [NotNullWhen(true)] string? update)
         => update is not null && string.Equals(original, update, StringComparison.OrdinalIgnoreCase) is false;
 }
