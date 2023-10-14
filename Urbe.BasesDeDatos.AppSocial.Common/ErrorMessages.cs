7ffff190929e9b71cb9cd7c6a8845daef127edea
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 02:06:23 -0400

    Create ErrorMessages.cs

---
+++ Urbe.BasesDeDatos.AppSocial.Common/ErrorMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace Urbe.BasesDeDatos.AppSocial.Common;
+
+public static class ErrorMessages
+{
+    public static ErrorMessage BadEmail(string email)
+        => new($"El correo electrónico está malformado: {email}");
+}
