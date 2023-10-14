3723a29e564b94f4fea68889b925c0fe6edb5350
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 02:33:23 -0400

    Update ErrorMessages.cs

--- Urbe.BasesDeDatos.AppSocial.Common/ErrorMessages.cs
+++ Urbe.BasesDeDatos.AppSocial.Common/ErrorMessages.cs
@@ -4,4 +4,7 @@ public static class ErrorMessages
 {
     public static ErrorMessage BadEmail(string email)
         => new($"El correo electrónico está malformado: {email}");
+
+    public static ErrorMessage TooLong(string property, int maxCharacters, int currentCharacters)
+        => new($"La propiedad {property} tiene demasiados caracteres ({currentCharacters}). Esta propiedad tiene un maximo de {maxCharacters} caracteres");
 }
