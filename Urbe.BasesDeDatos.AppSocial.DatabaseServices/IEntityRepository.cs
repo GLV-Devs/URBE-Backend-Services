7913bdbd07f68726aeb162c9c926b04deca80803
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 14:50:12 -0400

    Implemented EntityRepository

--- Urbe.BasesDeDatos.AppSocial.DatabaseServices/IEntityRepository.cs
+++ Urbe.BasesDeDatos.AppSocial.DatabaseServices/IEntityRepository.cs
@@ -7,6 +7,6 @@ public interface IEntityRepository<TEntity, TKey>
     where TEntity : IEntity, IKeyed<TKey>
     where TKey : struct, IEquatable<TKey>
 {
-    public ValueTask<TEntity> Find(SocialAppUser Requester, TKey key);
+    public ValueTask<TEntity?> Find(SocialAppUser Requester, TKey key);
     public IQueryable<TEntity> Query(SocialAppUser Requester);
 }
---
+++ Urbe.BasesDeDatos.AppSocial.DatabaseServices/Implementations/EntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Urbe.BasesDeDatos.AppSocial.Entities;
+using Urbe.BasesDeDatos.AppSocial.Entities.Interfaces;
+using Urbe.BasesDeDatos.AppSocial.Entities.Models;
+
+namespace Urbe.BasesDeDatos.AppSocial.DatabaseServices.Implementations;
+
+public class EntityRepository<TEntity, TKey> : IEntityRepository<TEntity, TKey>
+    where TEntity : class, IEntity, IKeyed<TKey>
+    where TKey : struct, IEquatable<TKey>
+{
+    protected readonly SocialContext context;
+
+    public EntityRepository(SocialContext context)
+    {
+        this.context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async ValueTask<TEntity?> Find(SocialAppUser Requester, TKey key)
+        => await context.Set<TEntity>().FirstOrDefaultAsync(x => x.Id.Equals(key));
+
+    public IQueryable<TEntity> Query(SocialAppUser Requester)
+        => context.Set<TEntity>();
+}
