96d08ddf9de2dd1120140e8e3d7a473013374ef6
Diego Garcia <dagarciam1014@gmail.com>
2023-10-13 22:58:07 -0400

    Create PendingMailConfirmation.cs

---
+++ Urbe.BasesDeDatos.AppSocial.Entities/Models/PendingMailConfirmation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Urbe.BasesDeDatos.AppSocial.Entities.Interfaces;
+
+namespace Urbe.BasesDeDatos.AppSocial.Entities.Models;
+
+public class PendingMailConfirmation : IEntity, IKeyed<RandomKey>, ISelfModelBuilder<PendingMailConfirmation>
+{
+    public RandomKeyId<PendingMailConfirmation> Id { get; init; }
+    RandomKey IKeyed<RandomKey>.Id => Id.Value;
+
+    public static void BuildModel(ModelBuilder modelBuilder, EntityTypeBuilder<PendingMailConfirmation> mb)
+    {
+        throw new NotImplementedException();
+    }
+}
