0c5066b1e0ba19d187444bc3ae6aaaff8ee1b6a7
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 00:02:27 -0400

    Update PostViewModel.cs

--- Urbe.BasesDeDatos.AppSocial.Entities/DTOs/PostViewModel.cs
+++ Urbe.BasesDeDatos.AppSocial.Entities/DTOs/PostViewModel.cs
@@ -3,8 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Urbe.BasesDeDatos.AppSocial.Entities.Models;
 
 namespace Urbe.BasesDeDatos.AppSocial.Entities.DTOs;
+
 public class PostViewModel
 {
+    public required SnowflakeId<Post> Id { get; init; }
+    public required GuidId<User> Poster { get; init; }
+    public required string Content { get; init; }
+    public required string PosterThenUsername { get; init; }
+    public required DateTimeOffset DatePosted { get; init; }
+    public required SnowflakeId<Post>? InResponseTo { get; init; }
+    public required HashSet<SnowflakeId<Post>>? Responses { get; init; }
+
+    public static PostViewModel FromPost(Post post)
+        => new()
+        {
+            Id = post.Id,
+            Poster = post.PosterId,
+            Content = post.Content,
+            PosterThenUsername = post.PosterThenUsername,
+            DatePosted = post.DatePosted,
+            InResponseTo = post.InResponseToId,
+            Responses = post.Responses?.Select(x => x.Id).ToHashSet()
+        };
 }
