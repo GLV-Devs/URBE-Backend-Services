92e3ea7ed59228fa3311b340d6fc0008078eea03
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 15:49:45 -0400

    Update ErrorMessages.cs

--- Urbe.BasesDeDatos.AppSocial.Common/ErrorMessages.cs
+++ Urbe.BasesDeDatos.AppSocial.Common/ErrorMessages.cs
@@ -7,4 +7,13 @@ public static class ErrorMessages
 
     public static ErrorMessage TooLong(string property, int maxCharacters, int currentCharacters)
         => new($"La propiedad {property} tiene demasiados caracteres ({currentCharacters}). Esta propiedad tiene un maximo de {maxCharacters} caracteres");
+
+    public static ErrorMessage NoPermission()
+        => new("El usuario no tiene permisos para realizar ésta acción");
+
+    public static ErrorMessage AlreadyInUse(string property, string value)
+        => new($"La propiedad {property} no puede contener el valor '{value}', debido a que ya esta siendo utilizada.");
+
+    public static ErrorMessage AlreadyInUseByOtherUser(string property, string value)
+        => new($"La propiedad {property} no puede contener el valor '{value}', debido a que ya esta siendo utilizada por otro usuario.");
 }
