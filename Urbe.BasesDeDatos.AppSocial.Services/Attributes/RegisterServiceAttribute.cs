3efe68923dd5c02ba1430c5035b17ea36424ad3e
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 03:19:43 -0400

    Introduced Services shared project

---
+++ Urbe.BasesDeDatos.AppSocial.Services/Attributes/RegisterServiceAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urbe.BasesDeDatos.AppSocial.Services.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public sealed class RegisterServiceAttribute : Attribute
+{
+    public Type Interface { get; }
+
+    public RegisterServiceAttribute(Type type)
+    {
+        Interface = type;
+    }
+}
---
+++ Urbe.BasesDeDatos.AppSocial.Services/ServicesHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Urbe.BasesDeDatos.AppSocial.Services.Attributes;
+
+namespace Urbe.BasesDeDatos.AppSocial.DatabaseServices;
+public static class ServicesHelper
+{
+    public static void RegisterDatabaseServices(this IServiceCollection serviceCollection)
+    {
+        foreach (var (type, attr) in AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Select(x => (Type: x, Attribute: x.GetCustomAttribute<RegisterServiceAttribute>()))
+                .Where(x => x.Attribute is not null))
+        {
+            Debug.Assert(attr is not null);
+            serviceCollection.AddScoped(attr.Interface, type);
+        }
+    }
+}
