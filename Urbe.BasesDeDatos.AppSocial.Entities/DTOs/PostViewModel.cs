be5051f4300a9c5c0d10570a91e5d90ce5a0231a
Diego Garcia <dagarciam1014@gmail.com>
2023-10-13 23:53:05 -0400

    Introduced user models

---
+++ Urbe.BasesDeDatos.AppSocial.Entities/DTOs/PostViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urbe.BasesDeDatos.AppSocial.Entities.DTOs;
+public class PostViewModel
+{
+}
---
+++ Urbe.BasesDeDatos.AppSocial.Entities/DTOs/UserCreationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urbe.BasesDeDatos.AppSocial.Entities.DTOs;
+
+public class UserCreationModel { }
+
+public class UserViewModel { }
+
+public class UserUpdateModel { }
+
+#error Not Implemented
