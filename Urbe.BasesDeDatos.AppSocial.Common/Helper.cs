02729b4d6afa9d1ade7a1c103fed9c62f06f6857
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 02:14:49 -0400

    Create Helper.cs

---
+++ Urbe.BasesDeDatos.AppSocial.Common/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urbe.BasesDeDatos.AppSocial.Common;
+public static class Helper
+{
+    public static bool IsUpdating(string original, [NotNullWhen(true)] string? update)
+        => update is not null && string.Equals(original, update, StringComparison.OrdinalIgnoreCase) is false;
+}
