ddc051de844254886f3c4c250e4033c440a2021f
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 16:47:29 -0400

    Split user view models

---
+++ Urbe.BasesDeDatos.AppSocial.DatabaseServices/DTOs/UserSelfViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Urbe.BasesDeDatos.AppSocial.Entities;
+using Urbe.BasesDeDatos.AppSocial.Entities.Models;
+
+namespace Urbe.BasesDeDatos.AppSocial.DatabaseServices.DTOs;
+
+public class UserSelfViewModel
+{
+    public required GuidId<SocialAppUser> Id { get; set; }
+    public required string Username { get; set; }
+    public required UserSettings Settings { get; set; }
+    public required string? ProfilePictureUrl { get; set; }
+    public string? RealName { get; set; }
+    public string? Pronouns { get; set; }
+    public string? ProfileMessage { get; set; }
+
+    public static UserSelfViewModel FromUser(SocialAppUser user)
+        => new()
+        {
+            Id = user.Id,
+            Username = user.UserName!,
+            Settings = user.Settings,
+            ProfilePictureUrl = user.ProfilePictureUrl,
+            RealName = user.RealName
+        };
+}
--- Urbe.BasesDeDatos.AppSocial.DatabaseServices/DTOs/UserViewModel.cs
+++ Urbe.BasesDeDatos.AppSocial.DatabaseServices/DTOs/UserViewModel.cs
@@ -1,25 +1,20 @@
-﻿using Urbe.BasesDeDatos.AppSocial.Entities;
-using Urbe.BasesDeDatos.AppSocial.Entities.Models;
+﻿using Urbe.BasesDeDatos.AppSocial.Entities.Models;
 
 namespace Urbe.BasesDeDatos.AppSocial.DatabaseServices.DTOs;
 
 public class UserViewModel
 {
-    public required GuidId<SocialAppUser> Id { get; set; }
     public required string Username { get; set; }
-    public required UserStatus Status { get; set; }
-    public required string? ProfilePictureUrl { get; set; }
-    public string? RealName { get; set; }
     public string? Pronouns { get; set; }
     public string? ProfileMessage { get; set; }
+    public string? RealName { get; set; }
+    public required string? ProfilePictureUrl { get; set; }
 
     public static UserViewModel FromUser(SocialAppUser user)
         => new()
         {
-            Id = user.Id,
             Username = user.UserName!,
-            Status = user.Status,
             ProfilePictureUrl = user.ProfilePictureUrl,
-            RealName = user.RealName
+            RealName = user.Settings.HasFlag(Entities.UserSettings.AllowRealNamePublicly) ? user.RealName : null
         };
 }
