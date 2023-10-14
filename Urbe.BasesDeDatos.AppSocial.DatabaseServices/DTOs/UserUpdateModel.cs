1438782cf348e7d9338a422f1a042249e2eb154b
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 15:49:59 -0400

    Removed password from model

--- Urbe.BasesDeDatos.AppSocial.DatabaseServices/DTOs/UserUpdateModel.cs
+++ Urbe.BasesDeDatos.AppSocial.DatabaseServices/DTOs/UserUpdateModel.cs
@@ -7,6 +7,5 @@ public class UserUpdateModel
     public string? Pronouns { get; set; }
     public string? ProfileMessage { get; set; }
     public string? Email { get; set; }
-    public string? Password { get; set; }
     public string? ProfilePictureUrl { get; set; }
 }
