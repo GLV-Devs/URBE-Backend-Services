5e38b331c624c07affee1645198c9af662cb21c7
Diego Garcia <dagarciam1014@gmail.com>
2023-10-13 23:53:46 -0400

    Create UserSession.cs

---
+++ Urbe.BasesDeDatos.AppSocial.Entities/Models/UserSession.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Identity.Client;
+using Urbe.BasesDeDatos.AppSocial.Entities.Interfaces;
+using Urbe.BasesDeDatos.AppSocial.Entities.Internal;
+
+namespace Urbe.BasesDeDatos.AppSocial.Entities.Models;
+
+public class UserSession : IEntity, IKeyed<RandomKey>, ISelfModelBuilder<UserSession>, IDeletable
+{
+    private readonly KeyedNavigation<Guid, User> UserNavigation = new();
+
+    public RandomKeyId<UserSession> Id { get; init; }
+    RandomKey IKeyed<RandomKey>.Id => Id.Value;
+
+    public User? User
+    {
+        get => UserNavigation.Entity;
+        init => UserNavigation.Entity = value;
+    }
+
+    public GuidId<User> UserId
+    {
+        get => UserNavigation.Id;
+        init => UserNavigation.Id = value.Value;
+    }
+
+    public DateTimeOffset CreationDate { get; init; }
+
+    public DateTimeOffset LastAccess { get; init; }
+
+    public static void BuildModel(ModelBuilder modelBuilder, EntityTypeBuilder<UserSession> mb)
+    {
+        mb.HasKey(x => x.Id);
+        mb.Property(x => x.Id).HasConversion(RandomKeyId<UserSession>.ValueConverter);
+        mb.HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId).IsRequired(true);
+    }
+
+    public ValueTask<bool> Delete(SocialContext context)
+    {
+        context.Remove(this);
+        return ValueTask.FromResult(true);
+    }
+}
