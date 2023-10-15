101b618f8fbd53d08aaa2c29cdc7f769645d017e
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 23:45:09 -0400

    Create SocialAppUserLoginModel.cs

---
+++ Urbe.BasesDeDatos.AppSocial.API/Models/Requests/SocialAppUserLoginModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Urbe.BasesDeDatos.AppSocial.API.Models.Requests;
+
+public class SocialAppUserLoginModel
+{
+    public string? UserNameOrEmail { get; set; }
+    public string? Password { get; set; }
+}
