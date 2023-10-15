be4740720a3b88fcf3114605a0aa1c26e93ee3ff
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 22:42:05 -0400

    Update Program.cs

--- Urbe.BasesDeDatos.AppSocial.API/Program.cs
+++ Urbe.BasesDeDatos.AppSocial.API/Program.cs
@@ -1,16 +1,22 @@
+using Urbe.BasesDeDatos.AppSocial.Services;
+using Serilog.Extensions.Hosting;
+using Serilog.Extensions.Logging;
+using Serilog;
+using Urbe.BasesDeDatos.AppSocial.Entities;
+using Urbe.BasesDeDatos.AppSocial.DatabaseServices.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Urbe.BasesDeDatos.AppSocial.Entities.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Urbe.BasesDeDatos.AppSocial.API;
 
 public static class Program
 {
+    public static WebApplication Application { get; }
+
     static Program()
     {
-
-    }
-
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(Environment.GetCommandLineArgs());
 
         // Add services to the container.
 
@@ -18,6 +24,32 @@ public static class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+
+        builder.Services.RegisterDecoratedServices();
+
+        builder.Logging.AddSerilog();
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        var dbconf = builder.Configuration.GetRequiredSection("DatabaseConfig").GetValue<DatabaseConfiguration>("SocialContext");
+        builder.Services.AddDbContext<SocialContext>(x =>
+        {
+            if (dbconf.DatabaseType is DatabaseType.SQLServer)
+            {
+                Log.Information("Registering SocialContext backed by SQLServer");
+                x.UseSqlServer(builder.Configuration.GetConnectionString("SocialContext"));
+            }
+            else if (dbconf.DatabaseType is DatabaseType.SQLite)
+            {
+                Log.Information("Registering SocialContext backed by SQLite");
+                x.UseSqlite(builder.Configuration.GetConnectionString("SocialContext"));
+            }
+        });
+
+        builder.Services.AddDefaultIdentity<SocialAppUser>()
 
         var app = builder.Build();
 
@@ -34,6 +66,9 @@ public static class Program
 
         app.MapControllers();
 
-        app.Run();
+        Application = app;
     }
+
+    public static Task Main(string[] args)
+        => Application.RunAsync();
 }
