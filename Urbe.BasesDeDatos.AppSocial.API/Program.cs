71297c768c75149bf0ef7a5dc2e9d3df69ba05a9
Diego Garcia <dagarciam1014@gmail.com>
2023-10-14 22:54:30 -0400

    Configured API

--- Urbe.BasesDeDatos.AppSocial.API/Program.cs
+++ Urbe.BasesDeDatos.AppSocial.API/Program.cs
@@ -7,6 +7,7 @@ using Urbe.BasesDeDatos.AppSocial.DatabaseServices.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Urbe.BasesDeDatos.AppSocial.Entities.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 
 namespace Urbe.BasesDeDatos.AppSocial.API;
 
@@ -49,7 +50,41 @@ public static class Program
             }
         });
 
-        builder.Services.AddDefaultIdentity<SocialAppUser>()
+        builder.Services.AddAuthentication(o =>
+        {
+            o.DefaultScheme = IdentityConstants.ApplicationScheme;
+            o.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+        })
+        .AddIdentityCookies(o => { });
+
+        builder.Services.AddIdentityCore<SocialAppUser>(o =>
+        {
+            o.Stores.MaxLengthForKeys = 128;
+
+            o.SignIn.RequireConfirmedEmail = true;
+
+            o.Password.RequireDigit = true;
+            o.Password.RequireNonAlphanumeric = true;
+            o.Password.RequiredLength = 8;
+            o.Password.RequiredUniqueChars = 5;
+
+            o.Lockout.MaxFailedAccessAttempts = 3;
+
+            o.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-";
+            o.User.RequireUniqueEmail = true;
+        })
+        .AddDefaultTokenProviders()
+        .AddEntityFrameworkStores<SocialContext>();
+
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            // Cookie settings
+            options.Cookie.HttpOnly = false;
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+
+            options.LoginPath = "/api/identity/account/login";
+            options.SlidingExpiration = true;
+        });
 
         var app = builder.Build();
 
@@ -59,9 +94,12 @@ public static class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+        else
+            app.UseHsts();
 
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
         app.MapControllers();
