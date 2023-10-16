dfa412806d20cb332fb49ae8ac06b2b9ca5de729
Diego Garcia <dagarciam1014@gmail.com>
2023-10-16 02:57:18 -0400

    Implemented UserController

--- Urbe.BasesDeDatos.AppSocial.API/Controllers/UserController.cs
+++ Urbe.BasesDeDatos.AppSocial.API/Controllers/UserController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.EntityFrameworkCore;
 using Urbe.BasesDeDatos.AppSocial.API.Controllers.Base;
 using Urbe.BasesDeDatos.AppSocial.DatabaseServices;
 using Urbe.BasesDeDatos.AppSocial.DatabaseServices.DTOs;
 using Urbe.BasesDeDatos.AppSocial.DatabaseServices.Implementations;
+using Urbe.BasesDeDatos.AppSocial.Entities;
 using Urbe.BasesDeDatos.AppSocial.Entities.Interfaces;
 using Urbe.BasesDeDatos.AppSocial.Entities.Models;
 
@@ -22,7 +27,105 @@ public class UserController : SocialAppController
         UserManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
     }
 
+    [HttpGet("query")]
+    [EnableQuery]
+    public async Task<ActionResult<IQueryable<UserViewModel>>> QueryUsers()
+    {
+        var u = await UserManager.GetUserAsync(User);
+        return Ok(UserRepository.Query(u));
+    }
+
+    [HttpPut("addfollow/{key}")]
+    [Authorize]
+    public async Task<IActionResult> FollowUser(Guid key)
+    {
+        var u = await UserManager.GetUserAsync(User);
+        Debug.Assert(u is not null);
+
+        var foundEntity = await UserRepository.Find(u, key);
+        return foundEntity is null ? NotFound() : await UserRepository.FollowUser(u, foundEntity) ? Ok() : BadRequest();
+    }
+
+    #region Regarding Requester and other user
+
+    [HttpGet("mutuals/{key}")]
+    [EnableQuery]
+    public async Task<IActionResult> GetMutuals(Guid key)
+    {
+        var u = await UserManager.GetUserAsync(User);
+
+        var foundEntity = await UserRepository.Find(u, key);
+        return foundEntity is null ? NotFound() : Ok(await UserRepository.GetViews(u, await UserRepository.GetMutuals(u, foundEntity)));
+    }
+
+    [HttpGet("followed/{key}")]
+    [EnableQuery]
+    public async Task<IActionResult> GetFollowed(Guid key)
+    {
+        var u = await UserManager.GetUserAsync(User);
+
+        var foundEntity = await UserRepository.Find(u, key);
+        return foundEntity is null ? NotFound() : Ok(await UserRepository.GetViews(u, await UserRepository.GetFollowing(u, foundEntity)));
+    }
+
+    [HttpGet("followers/{key}")]
+    [EnableQuery]
+    public async Task<IActionResult> GetFollowers(Guid key)
+    {
+        var u = await UserManager.GetUserAsync(User);
+        
+        var foundEntity = await UserRepository.Find(u, key);
+        return foundEntity is null ? NotFound() : Ok(await UserRepository.GetViews(u, await UserRepository.GetFollowers(u, foundEntity)));
+    }
+
+    [HttpGet("{key}")]
+    public async Task<IActionResult> ViewEntity(Guid key)
+    {
+        var u = await UserManager.GetUserAsync(User);
+        var foundEntity = await UserRepository.Find(u, key);
+        if (foundEntity is null)
+            return NotFound();
+
+        var viewresult = await UserRepository.GetView(u, foundEntity);
+        return viewresult.TryGetResult(out var view) ? Ok(view) : FailureResult(viewresult);
+    }
+
+    #endregion
+
+    #region Regarding Requester
+
+    [HttpGet("mutuals")]
+    [EnableQuery]
+    [Authorize]
+    public async Task<IActionResult> GetMutuals()
+    {
+        var u = await UserManager.GetUserAsync(User);
+        Debug.Assert(u is not null);
+        return Ok(await UserRepository.GetViews(u, await UserRepository.GetMutuals(u)));
+    }
+
+    [HttpGet("followed")]
+    [EnableQuery]
+    [Authorize]
+    public async Task<IActionResult> GetFollowed()
+    {
+        var u = await UserManager.GetUserAsync(User);
+        Debug.Assert(u is not null);
+        return Ok(await UserRepository.GetViews(u, await UserRepository.GetFollowing(u)));
+    }
+
+    [HttpGet("followers")]
+    [EnableQuery]
+    [Authorize]
+    public async Task<IActionResult> GetFollowers()
+    {
+        var u = await UserManager.GetUserAsync(User);
+        Debug.Assert(u is not null);
+        return Ok(await UserRepository.GetViews(u, await UserRepository.GetFollowers(u)));
+    }
+
     [HttpPut]
+    [Authorize]
     public async Task<IActionResult> Update([FromBody] UserUpdateModel update)
     {
         var u = await UserManager.GetUserAsync(User);
@@ -43,7 +146,8 @@ public class UserController : SocialAppController
     }
 
     [HttpGet]
-    public virtual async Task<IActionResult> ViewEntity()
+    [Authorize]
+    public async Task<IActionResult> ViewEntity()
     {
         var u = await UserManager.GetUserAsync(User);
         if (u is null)
@@ -52,4 +156,6 @@ public class UserController : SocialAppController
         var viewresult = await UserRepository.GetView(u, u);
         return viewresult.TryGetResult(out var view) ? Ok(view) : FailureResult(viewresult);
     }
+
+    #endregion
 }
