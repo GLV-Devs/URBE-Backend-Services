3d48c1024c792481df12041018d6d91458c53c88
Diego Garcia <dagarciam1014@gmail.com>
2023-10-16 16:49:15 -0400

    Configured OData

--- Urbe.BasesDeDatos.AppSocial.API/Program.cs
+++ Urbe.BasesDeDatos.AppSocial.API/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Urbe.BasesDeDatos.AppSocial.Entities.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.OData;
 
 namespace Urbe.BasesDeDatos.AppSocial.API;
 
@@ -21,7 +22,8 @@ public static class Program
 
         // Add services to the container.
 
-        builder.Services.AddControllers();
+        builder.Services.AddControllers().AddOData(o => o.Select().Filter().OrderBy().Count().SetMaxTop(100));
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
