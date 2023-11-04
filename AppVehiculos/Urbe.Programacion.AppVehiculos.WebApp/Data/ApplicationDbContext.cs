b83bc01e674a788f3314b00169bb87d5e89cc1c6
Diego Garcia <dagarciam1014@gmail.com>
2023-11-04 02:16:52 -0400

    Introduced AppVehiculos.WebApp

---
+++ AppVehiculos/Urbe.Programacion.AppVehiculos.WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Urbe.Programacion.AppVehiculos.WebApp.Data;
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
