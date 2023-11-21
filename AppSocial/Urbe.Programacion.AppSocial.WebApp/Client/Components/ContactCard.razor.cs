093d18eedbcdf12ea60e8dac4f0553c5c3141b83
MaferVil <maria.viloria200329@gmail.com>
2023-11-20 23:50:50 -0400

    Nuevo componente
    Componente ContactCard

---
+++ AppSocial/Urbe.Programacion.AppSocial.WebApp/Client/Components/ContactCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Urbe.Programacion.AppSocial.DataTransfer.Responses;
+
+namespace Urbe.Programacion.AppSocial.WebApp.Client.Components
+{
+    public partial class ContactCard
+    {
+        [Parameter]
+        public UserViewModel? UserModel { get; set; } = new();
+    }
+}
