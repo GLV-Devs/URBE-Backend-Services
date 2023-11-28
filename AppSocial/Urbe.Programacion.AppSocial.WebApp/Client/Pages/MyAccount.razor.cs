73fedca95128aec676164d00186a9ad61e10e7dc
Diego Garcia <dagarciam1014@gmail.com>
2023-11-28 02:22:37 -0400

    Cambios

---
+++ AppSocial/Urbe.Programacion.AppSocial.WebApp/Client/Pages/MyAccount.razor.cs
@@ -0,0 +1,8 @@
+﻿using Urbe.Programacion.Shared.Common;
+
+namespace Urbe.Programacion.AppSocial.WebApp.Client.Pages;
+
+public partial class MyAccount
+{
+    protected ErrorList Errors;
+}
--- AppSocial/Urbe.Programacion.AppSocial.WebApp/Client/WebHelper.cs
+++ AppSocial/Urbe.Programacion.AppSocial.WebApp/Client/WebHelper.cs
@@ -11,10 +11,13 @@ namespace Urbe.Programacion.AppSocial.WebApp.Client;
 
 public static class WebHelper
 {
-    public static string GetUserProfilePic(this UserViewModel user)
+    public static string RenderPostTime(this DateTimeOffset dto)
+        => dto.ToLocalTime().DateTime.ToString("dd/MM/yy hh:mm tt");
+
+    public static string GetUserProfilePic(this UserViewModel? user)
         => user?.ProfilePictureUrl ?? "/defaultuserpic.jpg";
 
-    public static string GetUserProfilePic(this UserSelfViewModel user)
+    public static string GetUserProfilePic(this UserSelfViewModel? user)
         => user?.ProfilePictureUrl ?? "/defaultuserpic.jpg";
 
     public static async ValueTask VerifyUserState(this AppState state, SocialApiClient client, NavigationManager nav, ILogger log, CancellationToken ct = default)
